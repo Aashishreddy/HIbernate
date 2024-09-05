7e36878ff654b62f9cf5c224240305ee9fe979ac
Aashish Reddy Vundhyala <avundhyala@apple.com>
2024-09-04 18:36:37 -0700

    Generic changes

--- src/main/java/com/example/hibernate/generics/GenericsJavaApplication.java
+++ src/main/java/com/example/hibernate/generics/GenericsJavaApplication.java
@@ -1,5 +1,10 @@
 package com.example.hibernate.generics;
 
+import com.example.hibernate.domain.entities.AuthorEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class GenericsJavaApplication {
     public static void main(String[] args) {
 
@@ -12,9 +17,35 @@ public class GenericsJavaApplication {
 
         printing("X");
         printing(50);
+        AuthorEntity createTestAuthor = AuthorEntity.builder().id(1234L).name("Aashish").age(25).build();
+        printing(createTestAuthor);
+
+        printing("X", createTestAuthor);
+        printing("Y", 26);
+
+        ArrayList<String> stringList = new ArrayList<>();
+        stringList.add("A");
+        stringList.add("B");
+
+        ArrayList<Integer> integerList = new ArrayList<>();
+        integerList.add(1);
+        integerList.add(2);
+
+        createGenericList(stringList);
+        createGenericList(integerList);
     }
 
     public static <T> void printing(T printEverything) {
         System.out.println(printEverything);
+    }
+
+    public static <T, V> void printing(T printEverything, V someThing) {
+        System.out.println(printEverything + " " + someThing);
+    }
+
+    public static void createGenericList(ArrayList<?> input) {
+        for(Object i: input) {
+            System.out.println(i);
+        }
     }
 }
