982c84d5abcf6e101f24e4f9af3ab35ac8b5f14e
Aashish Reddy Vundhyala <avundhyala@apple.com>
2024-09-05 12:28:54 -0700

    Functional Programming Streams and Filter

---
+++ src/main/java/com/example/hibernate/functionalprogramming/PersonIterator.java
@@ -0,0 +1,37 @@
+package com.example.hibernate.functionalprogramming;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PersonIterator {
+
+    public static void main(String[] args) {
+        Person p1 = Person.builder().name("Aashish").gender("MALE").build();
+        Person p2 = Person.builder().name("Bob").gender("FEMALE").build();
+        Person p3 = Person.builder().name("David").gender("MALE").build();
+        Person p4 = Person.builder().name("Jack").gender("FEMALE").build();
+
+        ArrayList<Person> peopleList = new ArrayList<>();
+        peopleList.add(p1);
+        peopleList.add(p2);
+        peopleList.add(p3);
+        peopleList.add(p4);
+
+        //To retrieve females
+        //APPROACH 1
+        System.out.println("Approach 1");
+        for(Person person : peopleList) {
+            if(person.getGender().equals("FEMALE")) {
+                System.out.println(person);
+            }
+        }
+
+        //APPROACH 2 using Streams and filters
+        System.out.println("Approach 2");
+        List<Person> females = peopleList.stream()
+                                            .filter(person -> person.getGender().equals("FEMALE"))
+                                            .collect(Collectors.toList());
+        females.forEach(System.out::println);
+    }
+}
